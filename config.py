5ee0778ada7cc0d36db04ab60a7e36c3f22251de
banten12517 <77748725+banten12517@users.noreply.github.com>
2021-01-21 01:51:44 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,95 @@
+Basic   ={
+        "Username"             : "xxxxxxxxx",
+        "Password"             : "xxxxxxxxx",
+
+        # Keep this OFF if your 2FA isn't activated
+        "2FA"                  : "OFF",
+        "BL-Key"               : "",
+        "Server"               : 3,
+
+        "Reset If Profit"      : 0.00000001,
+        "Reset If Lose"        : 0,
+        "Reset If Win Streak"  : 1,
+        "Reset If Lose Streak" : 0,
+
+        "Target Balance"       : 25000
+        }
+
+Betting  ={
+        "Base Bet"     : 0.01000037,
+        "Max Base Bet" : 0,
+        "Chance"       :{
+                        "Min": 5,
+                        "Max": 5
+                        },
+
+        "HiLo"         : "AUTO",  # AUTO / H / L
+
+        "If Win"       : 1,
+        "If Lose"      : 1.057,
+        "Number Bets"  : 0,    # Before excecuting Jump Set
+        "Jump Status"  : "on",
+        "Jump Set"     :[
+                {
+                "Comment"       : "tuyul",
+                "Status"        : "on",
+                "Base Bet"      : 0.01000378,
+                "Repeat Bets"   : 87,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                },{
+                "Comment"       : "tuyulwa",
+                "Status"        : "on",
+                "Base Bet"      : 0.05,
+                "Repeat Bets"   : 19,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                },{
+                "Comment"       : "tuyulu",
+                "Status"        : "on",
+                "Base Bet"      : 0.1,
+                "Repeat Bets"   : 9,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                },{
+                "Comment"       : "tuyupat",
+                "Status"        : "on",
+                "Base Bet"      : 0.15,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 5,
+                           "Max": 5
+                           }
+                }
+            ]
+        }
+
+ClientSeed ={
+            "Seed"           : 378995,
+            "Auto"           : "ON",
+            "If Win Streak"  : 1,
+            "If Lose Streak" : 9
+            }
+
+Extra   ={
+        "Target Profit"            : 3.78,
+        "Target Lose"              : 0,
+
+        "Action"               : "Relogin",  # Sleep or ReLogin
+        "Delay"                : 3
+        }
+#•••••••••••••••••••••••••••••••••••••••••••••••
+#  PARKIRAN JUARA
+#  Martingale (999dice) v1.0.0 (192)
+#  --
+#
+#  #abauji
+#  @iksano@
+#  Parkiranjuara@gmail.com
+#•••••••••••••••••••••••••••••••••••••••••••••••
